0f6a5621eb0df6bf0db065f03bf7b44b7e9ecbea
pabloandimu <pablo.mujica.trial@gmail.com>
2022-07-15 13:57:49 -0300

    Primer intento de commit

--- checkpoint.js
+++ checkpoint.js
@@ -7,7 +7,7 @@
 // métodos o construir determinadas funciones explicados más abajo.
 // Pero todos los métodos ya implementados en las homeowrks no es 
 // necesario que los vuelvan a definir.
-
+// COMENTARIO PRUEBA
 const {
     Queue,
     LinkedList,
